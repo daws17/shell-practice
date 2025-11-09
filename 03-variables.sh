3eb37d34e4a2b9c16bd6da7c56db080a41af7e52
sai kiran <saikiranakula17@gmail.com>
2025-11-10 02:09:46 +0530

    variable

--- 03-variables.sh
+++ 03-variables.sh
@@ -8,4 +8,4 @@ echo "$PERSON1:: hi $PERSON2, how are you?"
 echo "$PERSON2:: I'm fine PERSON1, thank you! how about you?"
 echo "$PERSON1:: I'm doing well. Thanks for asking!"
 echo "$PERSON1:: what r u learning"                                                                                                                                                                                                            
-echo "$PERSON2:: i'm learning scripting
+echo "$PERSON2:: i'm learning scripting"
