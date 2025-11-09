d433684a41650e8a40d37a5cce5be99de0f34a92
sai kiran <saikiranakula17@gmail.com>
2025-11-10 03:26:20 +0530

    variable

--- 04-variable.sh
+++ 04-variable.sh
@@ -2,7 +2,7 @@
 
 PERSON1=$1
 PERSON2=$2
-PERSON3=$3
+
 
 echo "$PERSON1:: hi $PERSON2, welcome to mumbai"
 echo "$PERSON2:: tq $PERSON1"
@@ -10,5 +10,3 @@ echo "$PERSON1:: when is ur match"
 echo "$PERSON2:: on Sunday
 echo "$PERSON1:: $PERSON2 where is surya?"
 echo "$PERSON2:: he is comming from his room"
-echo "$PERSON3:: hi $PERSON1 im here"
-echo "$PERSON1:: go nd practice $PERSON3 all the best for ur matchs"
