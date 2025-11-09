b1aa16384bd0cbce2880ea2a5e841f54d21c1ebb
sai kiran <saikiranakula17@gmail.com>
2025-11-10 03:31:14 +0530

    variable

--- 04-variable.sh
+++ 04-variable.sh
@@ -7,6 +7,6 @@ PERSON2=$2
 echo "$PERSON1:: hi $PERSON2, welcome to mumbai"
 echo "$PERSON2:: tq $PERSON1"
 echo "$PERSON1:: when is ur match"
-echo "$PERSON2:: on Sunday
+echo "$PERSON2:: on Sunday"
 echo "$PERSON1:: $PERSON2 where is surya?"
 echo "$PERSON2:: he is comming from his room"
