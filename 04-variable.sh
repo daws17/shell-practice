6870c2d6df955e593499b4d9b2855876dd358bc1
sai kiran <saikiranakula17@gmail.com>
2025-11-10 03:12:46 +0530

    variableS

---
+++ 04-variable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "$PERSON1:: hi $PERSON2, welcome to mumbai"
+echo "$PERSON2:: tq $PERSON1"
+echo "$PERSON1:: when is ur match"
+echo "$PERSON2:: on Sunday
+echo "$PERSON1:: $PERSON2 where is surya?"
+echo "$PERSON2:: he is comming from his room"
+echo "$PERSON3:: hi $PERSON1, i'm here"
+echo "$PERSON1:: go nd practice $PERSON3 all the best for ur matchs"
