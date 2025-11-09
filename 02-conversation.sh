2549c1641b0628874a28571f6dc05c037923bd34
sai kiran <saikiranakula17@gmail.com>
2025-11-10 01:46:38 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh:: hi Suresh, how are you?"
+echo "Suresh:: I'm fine Ramesh, thank you! how about you?"
+echo "Ramesh:: I'm doing well. Thanks for asking!"
+echo "Suresh:: what r u learning"                                                                                                                                                                                                            
+echo "Ramesh:: i'm learning scripting"
