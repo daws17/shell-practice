7c7452e02495396e0b63d3b1d8666c2f559494d2
sai kiran <saikiranakula17@gmail.com>
2025-11-17 20:27:00 +0530

    variables

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -2,4 +2,10 @@
 
 echo "exit status :$?"
 echo "script name : $0"
-echo "all variables passed to the script : $@"
+echo "all variables passed to the script : $@ or $*"
+echo "script name :$0"
+echo "current directory :$pwd"
+echo "who is running this script : $USER"
+echo "home directory of the user : $HOME"
+echo "process id of the current script (PID) : $$"
+echo "last background process id : $!"
