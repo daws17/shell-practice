d4aeda44a96dbbd4a93be8c386bea37c64ba7eb0
sai kiran <saikiranakula17@gmail.com>
2025-11-19 20:10:01 +0530

    data types

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -3,8 +3,9 @@
 #everything in shell considered as string
 NUMBER1=100
 NUMBER2=200
+NAME=devops
 
 #forcommand excute syntax is "$()"
-sum=$((NUMBER1+NUMBER2))
+sum=$((NUMBER1+NUMBER2+NAME))
 
 echo "sum is : ${sum}"
