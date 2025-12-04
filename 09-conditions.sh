3f6418dd3e4326fb56d36c7735343ab2021fbe59
sai kiran <saikiranakula17@gmail.com>
2025-12-04 21:05:16 +0530

    practice condition

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,5 +5,5 @@ NUMBER=$1
 if [ $NUMBER -lt 10 ]; then
     echo "given number $NUMBER is less then 10"
 else
-    echo "given number $NUMBER is greater then or equal to 10"
+    echo "given number $NUMBER is greater then 10"
 fi
