8bacd53b3f8796335e3dcb115cbd24d0ee48e228
sai kiran <saikiranakula17@gmail.com>
2025-12-11 20:45:41 +0530

    logs

--- 16-logs.sh
+++ 16-logs.sh
@@ -6,7 +6,7 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/Var/log/shell-script"
-SCRIPT_NAME=$ ( echo $0 | cut -d "." -f1)
+SCRIPT_NAME= $(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
