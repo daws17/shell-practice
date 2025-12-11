b278edaadf1c227107026cd60e052279eb779177
sai kiran <saikiranakula17@gmail.com>
2025-12-11 20:59:29 +0530

    logs

--- 16-logs.sh
+++ 16-logs.sh
@@ -6,13 +6,13 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/Var/log/shell-script"
-SCRIPT_NAME= $(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "script started excuted at : $(date)"
 
-if [ USERID -ne 0 ];then
+if [ $USERID -ne 0 ];then
     echo "ERROR:: please run this script with root privilage"
     exit1
 fi
